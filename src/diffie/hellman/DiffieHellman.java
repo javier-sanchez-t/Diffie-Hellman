6b4d1615c9e5b1252000def2bca22618b2636b9b
Javier Sánchez <jefe10jav@gmail.com>
2017-05-27 18:57:29 -0500

    #2 Format to print values

--- src/diffie/hellman/DiffieHellman.java
+++ src/diffie/hellman/DiffieHellman.java
@@ -11,9 +11,9 @@ public class DiffieHellman {
         Scanner sc = new Scanner(System.in);
         SecureRandom rnd = new SecureRandom();
 
-        System.out.println("+================================================+");
-        System.out.println("|                Diffie-Hellman                  |");
-        System.out.println("+================================================+");
+        System.out.println("+========================================================+");
+        System.out.println("|                    Diffie-Hellman                      |");
+        System.out.println("+========================================================+");
 
         //Input the bits
         System.out.print("Input the bit size: ");
@@ -43,22 +43,34 @@ public class DiffieHellman {
         Alice.calculateFinalKey();
         Bob.calculateFinalKey();
 
-        System.out.println("====================================================");
-        System.out.println("The value of p selected:________________" + p);
-        System.out.println("The value of g selected:________________" + g);
-        System.out.println("The value of a selected by Alice:_______" + Alice.getNumberChoosen());
-        System.out.println("The value of b selected by Bob:_________" + Bob.getNumberChoosen());
-        System.out.println("The value of A sent to Bob by Alice:____" + Bob.getNumberReceived());
-        System.out.println("The value of B sent to Alice by Bob:____" + Alice.getNumberReceived());
+        //Print results
+        System.out.println("=========================================================");
+        String pSelected = "The value of p selected:";
+        String gSelected = "The value of g selected:";
+        String aSelected = "The value of a selected by Alice:";
+        String bSelected = "The value of b selected by Bob:";
+        String ASelected = "The value of A sent to Bob by Alice:";
+        String BSelected = "The value of B sent to Alice by Bob:";
+        String AlicesName = "Alice:";
+        String BobsName = "Bob:";
+
+        System.out.printf("%-50s %-15s %n", pSelected, p);
+        System.out.printf("%-50s %-15s %n", gSelected, g);
+        System.out.printf("%-50s %-15s %n", aSelected, Alice.getNumberChoosen());
+        System.out.printf("%-50s %-15s %n", bSelected, Bob.getNumberChoosen());
+        System.out.printf("%-50s %-15s %n", ASelected, Bob.getNumberReceived());
+        System.out.printf("%-50s %-15s %n", BSelected, Alice.getNumberReceived());
         System.out.println("The value of key shared between Alice and Bob: ");
-        System.out.println("Alice:__" + Alice.getFinalKey());
-        System.out.println("Bob:____" + Bob.getFinalKey());
+        System.out.printf("%-50s %-15s %n", AlicesName, Alice.getFinalKey());
+        System.out.printf("%-50s %-15s %n", BobsName, Bob.getFinalKey());
+        System.out.println("==========================================================");
+        
         if (Alice.getFinalKey().equals(Bob.getFinalKey())) {
-            System.out.println(">>>  SUCCESS ALICE AND BOB HAVE THE SAME KEY!  :D <<<"); 
-       }else{
+            System.out.println(">>>  SUCCESS ALICE AND BOB HAVE THE SAME KEY!  :D <<<");
+        } else {
             System.err.println(">>>  ERROR ALICE AND BOB NOT HAVE THE SAME KEY!  :( <<<");
         }
-        System.out.println("====================================================");
+        
     }
 
 }
